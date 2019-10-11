5e19254c84da11c0f777fdd06c2fab2c52e36ce0
Lee Wei Jie <lee.weijie@dhs.sg>
2019-10-12 00:11:11 +0800

    Revamp FaceDetect to allow movement in > 1 axis

--- Projects/FaceDetect/FaceDetect.py
+++ Projects/FaceDetect/FaceDetect.py
@@ -17,255 +17,207 @@ import av
 # In[ ]:
 
 
-# Constants to access pre-trained models
-CAFFE_PROTOTXT = "deploy.prototxt.txt"
-CAFFE_MODEL = "face.caffemodel"
-CONFIDENCE = 0.5
-
-# Global variables to check state of drone
-movement_state = "standby"
-drone_lock = False
-
-
-# In[ ]:
-
-
-# Set up machine learning model
-net = cv2.dnn.readNetFromCaffe(CAFFE_PROTOTXT, CAFFE_MODEL)
-
-
-# In[ ]:
-
-
-# Function to stop movement of drone
-def drone_stop():
-    global movement_state
-    if movement_state == "left":
-        drone.left(0)
-    elif movement_state == "right":
-        drone.right(0)
-    elif movement_state == "front":
-        drone.forward(0)
-    elif movement_state == "back":
-        drone.backward(0)
-    elif movement_state == "up":
-        drone.up(0)
-    elif movement_state == "down":    
-        drone.down(0)
-
+class DroneWrapper:
+    def __init__(self):
+        # Set up pre-trained model for faces
+        CAFFE_PROTOTXT = "deploy.prototxt.txt"
+        CAFFE_MODEL = "face.caffemodel"
+        self.model = cv2.dnn.readNetFromCaffe(CAFFE_PROTOTXT, CAFFE_MODEL)
+        # Minimum threshold for faces
+        self.CONFIDENCE = 0.5
         
-# Threaded function to move drone
-def drone_move(move):
-    # Access global variable
-    global drone_lock
-    SPEED = 10
-    
-    # Stop the drone's previous motion before performing a different one
-    drone_stop()
-    # Prevent any further commands to the drone in the meantime
-    drone_lock = True
-    
-    # Give the drone time to stop
-    time.sleep(0.1)
-    
-    # Perform new movement
-    if move == "front":
-        drone.forward(SPEED)
-    elif move == "back":
-        drone.backward(SPEED)
-    elif move == "down":
-        drone.down(SPEED*2)
-    elif move == "up":
-        drone.up(SPEED*2)
-    elif move == "left":
-        drone.left(SPEED)
-    elif move == "right":
-        drone.right(SPEED)
+        # Drone movement states
+        # Up & down
+        self.throttle = 0
+        # Forward & backward
+        self.pitch = 0
+        # Right & left
+        self.roll = 0
+        
+        # Set up drone object
+        self.drone = tellopy.Tello()
+        self.drone.set_loglevel(0)
+        
+    # Connect to drone & set up video feed    
+    def connect(self):
+        # Put drone in command mode
+        self.drone.connect()
+        self.drone.wait_for_connection(3)
+        # Turn on video feed of drone and access it
+        self.drone.start_video()
+        retry = 3
+        self.video = None
+        while self.video is None and retry > 0:
+            retry -= 1
+            try:
+                self.video = av.open(self.drone.get_video_stream())
+                self.video.streams.video[0].thread_type = 'AUTO'
+            except av.AVError as ave:
+                print(ave)
+                print('retry...')
 
-    # Give the drone time to move
-    if move != "standby":
-        time.sleep(0.1)
-    
-    # Everything's done, ready for next move
-    drone_lock = False
+    def takeoff(self):
+        self.drone.takeoff()
+        # Move a little higher
+        time.sleep(4)
+        self.drone.up(30)
+        time.sleep(3)
+        self.drone.up(0)
+        # Start thread to update drone movement periodically
+        t = threading.Thread(target=self.update_drone_movement)
+        t.start()
+        
+    def land(self):
+        self.drone.land()
+        
+    def set_next_move(self, area, cx, cy, w, h):
+        # Backwards/forwards sensitivity
+        REF_AREA = 15000
+        AREA_BUFFER = 2500
+        MIN_AREA = 5000
 
-    
-# Function to calculate where the drone should move next
-def get_move(area, cx, cy, w, h):
-    # Backwards/forwards sensitivity
-    REF_AREA = 15000
-    AREA_BUFFER = 2500
-    MIN_AREA = 5000
-
-    # Left/right sensitivity
-    REF_X = w//2
-    REF_Y = h//2
-    TRANSLATE_BUFFER = 100
-    
-    # Move back
-    if area > REF_AREA + AREA_BUFFER:
-        return "back"
-    # Move front
-    elif MIN_AREA < area < REF_AREA - AREA_BUFFER:
-        return "front"
-    # Move down
-    elif cy > REF_Y + TRANSLATE_BUFFER:
-        return "down"
-    # Move up
-    elif cy < REF_Y - TRANSLATE_BUFFER:
-        return "up"
-    # Move right
-    elif cx > REF_X + TRANSLATE_BUFFER:
-        return "right"
-    # Move left
-    elif cx < REF_X - TRANSLATE_BUFFER:
-        return "left"
-    # Stop moving
-    else:
-        return "standby"
-
-
-# In[ ]:
-
-
-# Set up drone
-drone = tellopy.Tello()
-drone.set_loglevel(0)
-
-
-# In[ ]:
-
-
-# Put drone in command mode
-drone.connect()
-drone.wait_for_connection(3)
-
-
-# In[ ]:
-
-
-# Turn on video feed of drone and access it
-drone.start_video()
-retry = 3
-container = None
-while container is None and retry > 0:
-    retry -= 1
-    try:
-        container = av.open(drone.get_video_stream())
-        container.streams.video[0].thread_type = 'AUTO'
-    except av.AVError as ave:
-        print(ave)
-        print('retry...')
-
-
-# In[ ]:
-
-
-# Takeoff & move to an appropriate height!
-drone.takeoff()
-time.sleep(4)
-drone.up(20)
-time.sleep(3)
-drone.up(0)
-
-
-# In[ ]:
-
-
-# Start displaying video feed and processing it
-running = True
-while running:
-    frame_skip = 300
-    try:
-        for frame in container.decode(video=0):
-            # Skip some initial frames
-            if frame_skip > 0:
-                frame_skip -= 1
-                continue
-
-            # Get an image from frame
-            image = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
-            # Resize the image
-            image = imutils.resize(image, width=800)
-            # Get dimensions of image
-            (h, w) = image.shape[:2]
-
-            # Convert image to blob and identify face with model
-            blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
-                                         (300, 300), (104.0, 177.0, 123.0))
-            net.setInput(blob)
-            detections = net.forward()
+        # Left/right sensitivity
+        REF_X = w//2
+        REF_Y = h//2
+        TRANSLATE_BUFFER = 100
+        
+        # Speed
+        TRANSLATIONAL_SPEED = 0.1
+        THROTTLE_SPEED = 0.2
+        
+        # Reset all movement values first
+        self.pitch = 0
+        self.throttle = 0
+        self.roll = 0
+        
+        # Move back
+        if area > REF_AREA + AREA_BUFFER:
+            self.pitch = -TRANSLATIONAL_SPEED
+        # Move front
+        elif MIN_AREA < area < REF_AREA - AREA_BUFFER:
+            self.pitch = TRANSLATIONAL_SPEED
+        
+        # Move down
+        if cy > REF_Y + TRANSLATE_BUFFER:
+            self.throttle = -THROTTLE_SPEED
+        # Move up
+        elif cy < REF_Y - TRANSLATE_BUFFER:
+            self.throttle = THROTTLE_SPEED
             
-            # Get closest face
-            largest_area = 0
-            # loop over the detections and find best
-            for i in range(0, detections.shape[2]):
-                # extract the confidence of the detection
-                confidence = detections[0, 0, i, 2]
-                # If confidence of detection is above our threshold
-                if confidence > CONFIDENCE:
-                    # compute the (x, y)-coordinates of the bounding box for the
-                    # object
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (x1, y1, x2, y2) = box.astype("int")
+        # Move right
+        if cx > REF_X + TRANSLATE_BUFFER:
+            self.roll = TRANSLATIONAL_SPEED
+        # Move left
+        elif cx < REF_X - TRANSLATE_BUFFER:
+            self.roll = -TRANSLATIONAL_SPEED
+            
+    def update_drone_movement(self):
+        while True:
+            self.drone.set_throttle(self.throttle)
+            self.drone.set_pitch(self.pitch)
+            self.drone.set_roll(self.roll)
+            # Wait before updating again to give drone time to process
+            time.sleep(0.2)
+        
+    def start_tracking(self):
+        self.takeoff()
+        running = True
+        while running:
+            frame_skip = 300
+            try:
+                for frame in self.video.decode(video=0):
+                    # Skip some initial frames
+                    if frame_skip > 0:
+                        frame_skip -= 1
+                        continue
 
-                    # Process the bounding box to find area
-                    area = (x2-x1)*(y2-y1)
-                    # If current detected face is the closest to drone
-                    if area > largest_area:
-                        largest_area = area
-                        # Find center point of face
-                        cx, cy = (x1+x2)//2, (y1+y2)//2
+                    # Get an image from frame
+                    image = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+                    # Resize the image
+                    image = imutils.resize(image, width=800)
+                    # Get dimensions of image
+                    (h, w) = image.shape[:2]
+
+                    # Convert image to blob and identify face with model
+                    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
+                                                 (300, 300), (104.0, 177.0, 123.0))
+                    self.model.setInput(blob)
+                    detections = self.model.forward()
+
+                    # Get closest face
+                    largest_area = 0
+                    # loop over the detections and find best
+                    for i in range(0, detections.shape[2]):
+                        # extract the confidence of the detection
+                        confidence = detections[0, 0, i, 2]
+                        # If confidence of detection is above our threshold
+                        if confidence > self.CONFIDENCE:
+                            # compute the (x, y)-coordinates of the bounding box for the
+                            # object
+                            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                            (x1, y1, x2, y2) = box.astype("int")
+
+                            # Process the bounding box to find area
+                            area = (x2-x1)*(y2-y1)
+                            # If current detected face is the closest to drone
+                            if area > largest_area:
+                                largest_area = area
+                                # Find center point of face
+                                cx, cy = (x1+x2)//2, (y1+y2)//2
+                            # Display bounding box around detected face
+                            cv2.rectangle(image, (x1, y1), (x2, y2),
+                                      (0, 0, 255), 2)
+
+                    # If we can find a face
+                    if largest_area != 0:
+                        # Get the next move the drone should make
+                        move = self.set_next_move(largest_area, cx, cy, w, h)
                         
-            # If we can find a face
-            if largest_area != 0:
-                # Get the next move the drone should make
-                move = get_move(area, cx, cy, w, h)
-                # If the drone is not already performing the move and
-                # drone is not busy
-                if not drone_lock and movement_state != move:
-                    # Move the drone in a separate thread as delay is involved
-                    thread = threading.Thread(target=drone_move, args=(move,))
-                    thread.start()
-                    # Record down the move for comparison in the next frame
-                    movement_state = move
+                    # If we cannot find any face
+                    else:
+                        self.pitch = 0
+                        self.throttle = 0
+                        self.roll = 0
+
+                    # Display movement info
+                    cv2.putText(image, "throttle(up-down): {}".format(self.throttle), (20, 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 0), 2)
+                    cv2.putText(image, "pitch(front-back): {}".format(self.pitch), (20, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 0), 2)
+                    cv2.putText(image, "roll(right-left): {}".format(self.roll), (20, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 0), 2)
                     
-                # Display movement state of drone
-                cv2.putText(image, movement_state, (cx, cy),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                    # Display current frame on screen
+                    cv2.imshow("image", image)
+                    key = cv2.waitKey(1) & 0xFF
 
-                # Display bounding box around detected face
-                cv2.rectangle(image, (x1, y1), (x2, y2),
-                              (0, 0, 255), 2)
-            # If we cannot find any face and drone is moving
-            elif movement_state != "standby":
-                drone_stop()
-                movement_state = "standby"
+                    # Land drone if "q" is pressed
+                    if key == ord("q"):
+                        self.land()
+                        running = False
+                        break
 
-            # Display current frame on screen
-            cv2.imshow("image", image)
-            key = cv2.waitKey(1) & 0xFF
-
-            # Land drone if "q" is pressed
-            if key == ord("q"):
-                drone.land()
-                running = False
-                break
-
-            # Skip some frames to keep up with drone
-            frame_skip = 5
-    except Exception as e:
-        print(e)
+                    # Skip some frames to keep up with drone
+                    frame_skip = 5
+            except Exception as e:
+                print(e)
 
 
 # In[ ]:
 
 
-drone.land()
+my_drone = DroneWrapper()
 
 
 # In[ ]:
 
 
+my_drone.connect()
+my_drone.start_tracking()
 
+
+# In[ ]:
+
+
+my_drone.land()
 
